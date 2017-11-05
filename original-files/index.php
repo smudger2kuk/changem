e0b503176de28ac7a9fc5bcd84deab9f83571fcc
Mark Smith <smudger2kuk@hotmail.com>
2017-11-05 16:20:42 +0000

    updated files

---
+++ original-files/index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+
+    <title>Change M - Change Management Consultancy Services</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-danger">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <a class="navbar-brand" href="#">Change M</a>
+        
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#about-us">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#our-services">Our Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact-us">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="jumbotron" id="about-us">
+
+            <h1>Business Transformation, Change and Programme Management</h1><br>
+            <h3>Helping you transform your business to realise your vision</h3><br>
+			<p>Providing tailored and results-driven consultancy to help your business successfully define, achieve and sustain major transformational change.</p>
+			<p>Enabling your organisation to define and achieve strategic objectives, establish appropriate governance and methods, embed new operating models, deliver new processes and systems solutions and carefully manage change and engagement.</p>
+			<p>Passionate about understanding your business to create ownership of change, collaborating to ensure that impact is carefully managed before, during and after change occurs.</p>
+			<p>Bringing experience of a range of programme and project models, including Managing Successful Programmes, Prince2, Agile and Lean, ensuring an effective and transparent approach delivery.
+			</p>
+            <p><a class="btn btn-danger" href="testimonials" role="button">Who we've worked with &raquo;</a></p>
+
+        </div>
+    </div>
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+        <h1 class="text-center text-danger services" id="our-services">Our Services</h1>
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+			<div class="col-md-4 text-center">
+                <img class="rounded-circle" src="icons/008-bubble.png" alt="project management training & coaching services icon" width="140" height="140">
+                <h3 class="text-danger">Vision, Strategy &amp; Planning</h3>
+            </div><!-- /.col-lg-4 -->
+            <div class="col-md-4 text-center">
+                <img class="rounded-circle" src="icons/032-document-1.png" alt="governance modelling service icon" width="140" height="140">
+                <h3 class="text-danger">Governance</h3>
+            </div><!-- /.col-lg-4 -->
+			<div class="col-md-4 text-center">
+                <img class="rounded-circle" src="icons/022-user-3.png" alt="change management services icon" width="140" height="140">
+                <h3 class="text-danger">Change Management</h3>
+            </div><!-- /.col-lg-4 -->			
+        </div><!-- /.row -->
+		<br>
+		<div class="row">
+            <div class="col-md-4 text-center">
+                <img class="rounded-circle" src="icons/035-user-2.png" alt="programme management services" width="140" height="140">
+                <h3 class="text-danger">Programme Management</h3>
+            </div><!-- /.col-lg-4 -->
+			<div class="col-md-4 text-center">
+                <img class="rounded-circle" src="icons/005-checklist-2.png" alt="project management services icon" width="140" height="140">
+                <h3 class="text-danger">Project Management</h3>
+            </div><!-- /.col-lg-4 -->
+			<div class="col-md-4 text-center">
+                <img class="rounded-circle" src="icons/037-conference-1.png" alt="project management training & coaching services icon" width="140" height="140">
+                <h3 class="text-danger">Training &amp; Coaching</h3>
+            </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+		
+		<div class="row featurette">
+            <div class="col-md-7 push-md-5">
+                <h2 class="featurette-heading text-danger">Vision, Strategy &amp; Planning</h2>
+                <p class="lead" id="governance-models">A strong governance framework is essential to achieve your strategic vision and objectives, but must also fit your culture and environment. We work with your existing organisational model to identify and embed effective governance to direct, assure and reduce risk in delivering change.</p>
+            </div>
+            <div class="col-md-5 pull-md-7">
+                <img class="featurette-image img-fluid mx-auto" src="icons/008-bubble.png" alt="Generic placeholder image">
+            </div>
+        </div>
+		
+		<div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading text-danger">Governance</h2>
+                <p class="lead" id="governance-models">A strong governance framework is essential to achieve your strategic vision and objectives, but must also fit your culture and environment. We work with your existing organisational model to identify and embed effective governance to direct, assure and reduce risk in delivering change.</p>
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto" src="icons/032-document-1.png" alt="Generic placeholder image">
+            </div>
+        </div>
+
+        <div class="row featurette">
+            <div class="col-md-7 push-md-5">
+                <h2 class="featurette-heading text-danger">Change Management</h2>
+                <p class="lead" id="change-management">No matter how good the vision or plan, no transformation will occur unless your staff, clients and suppliers understand, embrace and adopt the changes. We consult to fully understand the impact of change and deploy methods, processes, systems, tools and activities to prepare, equip and support everyone through the transition.</p>
+            </div>
+            <div class="col-md-5 pull-md-7">
+                <img class="featurette-image img-fluid mx-auto" src="icons/022-user-3.png" alt="Generic placeholder image">
+            </div>
+        </div>
+		
+		<div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading text-danger">Programme Management</h2>
+                <p class="lead" id="programme-management">Applying the Managing Successful Programmes Framework, we define, resource, establish and manage an effective Programme to deliver clear solutions, outcomes and benefits with effective gateways, risk review and careful management of dependencies.</p>
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto" src="icons/035-user-2.png" alt="Generic placeholder image">
+            </div>
+        </div>
+
+        <div class="row featurette">
+            <div class="col-md-7 push-md-5">
+                <h2 class="featurette-heading text-danger">Project Management</h2>
+                <p class="lead" id="project-management">Whether component projects of a Programme or smaller standalone initiatives, we ensure you have the appropriate skills, methods and controls in place to define and manage project delivery, applying Prince 2 or Agile methodologies as appropriate.</p>
+            </div>
+            <div class="col-md-5 pull-md-7">
+                <img class="featurette-image img-fluid mx-auto" src="icons/005-checklist-2.png" alt="Generic placeholder image">
+            </div>
+        </div>
+
+        <div class="row featurette">
+            <div class="col-md-7">
+                <h2 class="featurette-heading text-danger">Training &amp; Coaching</h2>
+                <p class="lead" id="coaching-training">Transform your Programme and Project Management capability with one-to-one coaching or group training, by an experienced MSP and Prince2 Practitioner and coach. We specialise in practical, bite-sized sessions covering a range of principles such as programme and project leadership, benefits management, product based planning, risk management, and more. </p>
+            </div>
+            <div class="col-md-5">
+                <img class="featurette-image img-fluid mx-auto" src="icons/037-conference-1.png" alt="Generic placeholder image">
+            </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <h2 class="featurette-heading text-danger text-center" id="contact-us">Contact Us</h2>
+        <p class="lead text-center" id="governance-models">For a no-obligation discussion on your business transformation or change needs, please get in touch: </p>
+                
+        <form id="contact-form" class="col-md-8 push-2" action="contactForm.php" method="post">
+
+            <div class="form-group">
+                <input type="text" class="form-control" name="name" id="name" placeholder="Your name" required>
+            </div>
+			
+			<div class="form-group">
+                <input type="text" class="form-control" name="company" id="company" placeholder="Your Organisation (if applicable)"> 
+            </div>
+			
+            <div class="form-group">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Enter your email" required> 
+            </div>
+            
+            <div class="form-group">
+                <input type="number" class="form-control" name="telephone" id="telephone" placeholder="Telephone number"> 
+            </div>
+
+            <div class="form-group">
+                <textarea class="form-control" name="message" id="message" placeholder="Your message" required></textarea>   
+            </div>
+
+            <button type="submit" id="submit" class="btn btn-danger btn-block"><i class="fa fa-envelope" aria-hidden="true"></i> <span id="larger-text">Send</span></button>
+
+        </form>
+            
+
+        <hr class="featurette-divider">
+        
+        <h2 class="featurette-heading text-danger text-center services" id="testimonials">Organisations we've worked with...</h2>
+        
+        <div class="row text-center">
+            <div class="col-md-3">
+                <img src="logos/amey-plc.png" alt="" height="140">
+            </div><!-- /.col-lg-3 -->
+            <div class="col-md-3">
+                <img src="logos/balfour-beatty.png" height="140">
+            </div><!-- /.col-lg-3 -->            
+            <div class="col-md-3">
+                <img src="logos/cfbt-education-trust.png" alt="" height="140">
+            </div><!-- /.col-lg-3 -->
+            <div class="col-md-3">
+                <img src="logos/williams-lea.png" alt="" height="140">
+            </div><!-- /.col-lg-3 -->
+        </div><!-- /.row -->
+        <br>
+        <div class="row text-center">
+            <div class="col-md-3">
+                <img src="logos/invesco-perpetual.jpg" alt="" height="140">
+            </div><!-- /.col-lg-3 -->
+            <div class="col-md-3">
+                <img src="logos/norwich-union.png" alt="" height="140">
+            </div><!-- /.col-lg-3 -->   
+			<div class="col-md-3">
+                <img src="logos/westminster-city-council.png" alt="" height="140">
+            </div><!-- /.col-lg-3 -->
+            <div class="col-md-3">
+                <img src="logos/university-salford.png" alt="" height="140">
+            </div><!-- /.col-lg-3 -->
+        </div><!-- /.row -->
+        
+        <hr class="featurette-divider">
+        
+        <!-- /END THE FEATURETTES -->
+
+
+        <!-- FOOTER -->
+        <footer>
+            <div class="row">
+                <div class="col-md-2 push-8">
+                    <p class="float-right"><a href="#about-us">Back to top</a></p>
+                </div>
+            </div>
+
+            <p class="text-center">&copy; 2017 ChangeM Ltd &middot; Company Reg. 10083266 &middot; <a href="#home">Swindon Web Design</a></p>
+
+        </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ original-files/page.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="">
+
+    <title>Change M - Change Management Consultancy Services</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-danger">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <a class="navbar-brand" href="#">Change M</a>
+        
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#about-us">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#our-services">Our Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact-us">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="jumbotron" id="about-us">
+
+            <h1>Business Transformation, Change and Programme Management</h1><br>
+            <h3>Helping you transform your business to realise your vision</h3><br>
+			<p>Providing tailored and results-driven consultancy to help your business successfully define, achieve and sustain major transformational change.</p>
+			<p>Enabling your organisation to define and achieve strategic objectives, establish appropriate governance and methods, embed new operating models, deliver new processes and systems solutions and carefully manage change and engagement.</p>
+			<p>Passionate about understanding your business to create ownership of change, collaborating to ensure that impact is carefully managed before, during and after change occurs.</p>
+			<p>Bringing experience of a range of programme and project models, including Managing Successful Programmes, Prince2, Agile and Lean, ensuring an effective and transparent approach delivery.
+			</p>
+            <p><a class="btn btn-danger" href="testimonials" role="button">Who we've worked with &raquo;</a></p>
+
+        </div>
+
+        <!-- FOOTER -->
+        <footer>
+            <div class="row">
+                <div class="col-md-2 push-8">
+                    <p class="float-right"><a href="#about-us">Back to top</a></p>
+                </div>
+            </div>
+
+            <p class="text-center">&copy; 2017 ChangeM Ltd &middot; Company Reg. 10083266 &middot; <a href="#home">Swindon Web Design</a></p>
+
+        </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"><\/script>')</script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+  </body>
+</html>
